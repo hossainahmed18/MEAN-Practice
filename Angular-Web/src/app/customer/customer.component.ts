a848c86be0278ebbd1f34e2d67392647ea1378b8
Hossain Ahmed <HAhmed@rensafamily.company>
2022-02-04 18:28:51 +0600

    crud done

--- Angular-Web/src/app/customer/customer.component.ts
+++ Angular-Web/src/app/customer/customer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Customer } from '../Interfaces/customer';
 import { ModalService } from '../modal/modal.service';
-import{ ApiService } from '../services/api.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-customer',
@@ -10,37 +10,98 @@ import{ ApiService } from '../services/api.service';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
-  loadingCustomers = 0;
-  customers!:Customer[];
+  apiCalling = 0;
+  customers!: Customer[];
   customerDeleteWarning = "customerDeleteWarning";
+  deleteEmail!: string;
+  formSubmitted = false;
+  saveMode :number = 0;
 
   customerForm: FormGroup = this.formBuilder.group({
-		name: ['',{ validators: [Validators.required] }],
-    email: ['',{ validators: [Validators.required] }],
-    phoneNumber: ['',{ validators: [Validators.required] }],
-	});
+    name: ['', { validators: [Validators.required] }],
+    email: ['', { validators: [Validators.required] }],
+    phoneNumber: ['', { validators: [Validators.required] }],
+  });
 
-  constructor(private apiService: ApiService, private formBuilder: FormBuilder, private modalService : ModalService) { }
+  constructor(private apiService: ApiService, private formBuilder: FormBuilder, private modalService: ModalService) { }
 
   ngOnInit(): void {
     this.loadCustomers();
   }
 
-  submitCustomerForm(){
-
+  submitCustomerForm() {
+    this.formSubmitted = true;
+    if (!this.customerForm.valid) {
+      return;
+    }
+    let customerData: Customer = {
+      email: this.customerForm.controls.email.value,
+      name: this.customerForm.controls.name.value,
+      phoneNumber: this.customerForm.controls.phoneNumber.value,
+    };
+    this.apiCalling =1;
+    if(this.saveMode == 1){
+      this.apiService.saveCustomer(customerData).subscribe((data: Customer) => {
+        this.customers = this.customers.splice(0, 0, data);
+        this.apiCalling = 2;
+        this.resetForm();
+        this.saveMode = 0;
+        this.formSubmitted = false;
+      })
+    }else if(this.saveMode == 2){
+      this.apiService.updateCustomer(customerData).subscribe((data: Customer) => {
+        this.customers = this.customers.filter(c => c.email != customerData.email);
+        this.customers = this.customers.splice(0, 0, data);
+        this.apiCalling = 2;
+        this.resetForm();
+        this.saveMode = 0;
+        this.formSubmitted = false;
+      })
+    }
+    
   }
 
-  loadCustomers(){
-    this.apiService.getCustomers().subscribe((customers:Customer[])=>{
+  loadCustomers() {
+    this.apiCalling =1;
+    this.apiService.getCustomers().subscribe((customers: Customer[]) => {
       this.customers = customers;
-   })
+      this.apiCalling =2;
+    })
   }
 
-  setForDelete(){
+  setForDelete(deleteEmail: string) {
+    this.deleteEmail = deleteEmail;
     this.modalService.open(this.customerDeleteWarning);
   }
-  closeModal(){
+  closeModal() {
     this.modalService.close(this.customerDeleteWarning);
   }
+  deleteRequest() {
+    this.apiCalling =1;
+    this.apiService.deleteCustomers(this.deleteEmail).subscribe((data: any) => {
+      this.customers = this.customers.filter(c => c.email != this.deleteEmail);
+      this.deleteEmail = '';
+      this.apiCalling =2;
+    })
+  }
 
+  setForEdit(customer:Customer) {
+    this.customerForm.setValue({
+      name: customer.name,
+      email: customer.email,
+      phoneNumber: customer.phoneNumber
+    });
+    this.saveMode = 2;
+  }
+  setForSave() {
+    this.saveMode = 1;
+  }
+
+  resetForm() {
+    this.customerForm.setValue({
+      name: '',
+      email: '',
+      phoneNumber: ''
+    });
+  }
 }
--- Angular-Web/src/app/services/api.service.ts
+++ Angular-Web/src/app/services/api.service.ts
@@ -20,4 +20,13 @@ export class ApiService {
   getCustomers(){
     return this.http.get<Customer[]>(environment.apiEndPoint+'/customer',this.httpOptions);
   }
+  deleteCustomers(deleteEmail:string){
+    return this.http.delete(environment.apiEndPoint+`/customer?email=${deleteEmail}`,this.httpOptions);
+  }
+  saveCustomer(customer:Customer){
+    return this.http.post<Customer>(environment.apiEndPoint+'/customer',customer,this.httpOptions);
+  }
+  updateCustomer(customer:Customer){
+    return this.http.put<Customer>(environment.apiEndPoint+'/customer/',customer,this.httpOptions);
+  }
 }
