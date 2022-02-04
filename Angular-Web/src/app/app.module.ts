532d4b3ad01d6b81fca0b140c20294af7cbd33fd
Hossain Ahmed <HAhmed@rensafamily.company>
2022-02-04 17:26:38 +0600

    modal set up with fontawesome icon

--- Angular-Web/src/app/app.module.ts
+++ Angular-Web/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular-Web/src/app/customer/customer.component.ts
+++ Angular-Web/src/app/customer/customer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Customer } from '../Interfaces/customer';
+import { ModalService } from '../modal/modal.service';
 import{ ApiService } from '../services/api.service';
 
 @Component({
@@ -9,14 +11,36 @@ import{ ApiService } from '../services/api.service';
 })
 export class CustomerComponent implements OnInit {
   loadingCustomers = 0;
-  customers!:Customer[]
+  customers!:Customer[];
+  customerDeleteWarning = "customerDeleteWarning";
 
-  constructor(private apiService: ApiService) { }
+  customerForm: FormGroup = this.formBuilder.group({
+		name: ['',{ validators: [Validators.required] }],
+    email: ['',{ validators: [Validators.required] }],
+    phoneNumber: ['',{ validators: [Validators.required] }],
+	});
+
+  constructor(private apiService: ApiService, private formBuilder: FormBuilder, private modalService : ModalService) { }
 
   ngOnInit(): void {
+    this.loadCustomers();
+  }
+
+  submitCustomerForm(){
+
+  }
+
+  loadCustomers(){
     this.apiService.getCustomers().subscribe((customers:Customer[])=>{
-       this.customers = customers;
-    })
+      this.customers = customers;
+   })
+  }
+
+  setForDelete(){
+    this.modalService.open(this.customerDeleteWarning);
+  }
+  closeModal(){
+    this.modalService.close(this.customerDeleteWarning);
   }
 
 }
